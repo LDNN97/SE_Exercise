a7149b66a5cc837d02101bc769860e64cc7cdf54
LDNN97 <ldnn97@foxmail.com>
2020-09-07 23:50:05 +0800

    cleanup

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ldnn97/Downloads/SE Project
-# Build directory: /Users/ldnn97/Downloads/SE Project/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
