d799e8ad6aa71d0bfdffc9b8a264d637e4e3278c
LDNN97 <ldnn97@foxmail.com>
2020-09-07 21:37:51 +0800

    Changes to be committed: 	new file:   .vscode/settings.json

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/ldnn97/Downloads/SE Project
+# Build directory: /Users/ldnn97/Downloads/SE Project/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
